83746887acf38e70ed15924176ba4212da61a86c
홍성문 <2020041412@hanyang.ac.kr>
2025-08-14 13:24:02 +0900

    feat: auto-time custom-lib base code

---
+++ auto-time/src/main/java/com/eatcloud/autotime/repository/SoftDeleteRepository.java
@@ -0,0 +1,24 @@
+package com.eatcloud.autotime.repository;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import com.eatcloud.autotime.BaseTimeEntity;
+
+/** 물리 삭제 금지. 항상 softDelete* 사용. */
+@NoRepositoryBean
+public interface SoftDeleteRepository<T extends BaseTimeEntity, ID extends Serializable>
+	extends JpaRepository<T, ID> {
+
+	void softDelete(T entity, String actor);
+	void softDeleteById(ID id, String actor);
+
+	// 벌크 UPDATE로 deleted*/ 세팅 (리스너 미탐)
+	int softDeleteAllByIds(Collection<ID> ids, String actor);
+
+	// 복구
+	int restoreAllByIds(Collection<ID> ids);
+}
