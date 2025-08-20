f891ceec6d87ce53ad24e4fae4fc84783644e972
강능요 <dharmavarsa@naver.com>
2025-08-20 14:31:35 +0900

    fix : schema.sql 수정

--- customer-service/src/main/resources/db/schema.sql
+++ customer-service/src/main/resources/db/schema.sql
@@ -2,8 +2,8 @@ CREATE SCHEMA IF NOT EXISTS customer;
 SET search_path TO customer;
 
 CREATE TABLE IF NOT EXISTS p_customer (
-                                          id           UUID PRIMARY KEY,
-                                          name         VARCHAR(20) UNIQUE NOT NULL,
+    id           UUID PRIMARY KEY,
+    name         VARCHAR(20) UNIQUE NOT NULL,
     nickname     VARCHAR(100),
     email        VARCHAR(255),
     password     VARCHAR(255) NOT NULL,
@@ -18,8 +18,8 @@ CREATE TABLE IF NOT EXISTS p_customer (
     );
 
 CREATE TABLE IF NOT EXISTS p_addresses (
-                                           id           UUID PRIMARY KEY,
-                                           customer_id  UUID NOT NULL REFERENCES p_customer(id),
+    id           UUID PRIMARY KEY,
+    customer_id  UUID NOT NULL REFERENCES p_customer(id),
     zipcode      VARCHAR(10),
     road_addr    VARCHAR(500),
     detail_addr  VARCHAR(200),
